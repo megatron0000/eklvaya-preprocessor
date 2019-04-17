3aec8ca634d5d9dcb3a3d51f267e3faf907fcea8
Vitor <vitor_pimenta97@hotmail.com>
2019-04-17 17:43:31 -0300

    Add call site conversion

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 http://www.capstone-engine.org/lang_python.html
 https://medium.com/sector443/python-for-reverse-engineering-1-elf-binaries-e31e92c33732
+
+
+pyelftools uses inconsistent DIE offsets when there is more than 1 CU.
+
+https://medium.com/sector443/python-for-reverse-engineering-1-elf-binaries-e31e92c33732
---
+++ main.py
@@ -0,0 +1,345 @@
+# -*- coding: utf-8 -*-.
+
+from __future__ import print_function
+from capstone.x86 import *
+from elftools.elf.elffile import ELFFile
+import sys
+from xprint import *
+
+from capstone import *
+
+import subprocess
+import re
+
+sys.path[0:0] = ['.', '..']
+
+
+def get_ELFFile(file_name):
+    f = open(file_name, 'rb')
+    return ELFFile(f)
+
+
+def get_dwarfinfo(elffile):
+    return elffile.get_dwarf_info()
+
+
+def print_debug(file_name):
+    elffile = get_ELFFile(file_name)
+    dwarfinfo = get_dwarfinfo(elffile)
+    diedict = get_offset2DIE(dwarfinfo)
+    disassemble = get_text_disassemble(elffile)
+    # print(dir(dwarfinfo))
+    # print(dir(dwarfinfo.structs))
+    print('init subprogram finding')
+    for compile_unit in dwarfinfo.iter_CUs():
+        for die in compile_unit.iter_DIEs():
+            # print('At offset ' + str(die.offset) + ':')
+            if (die.tag == 'DW_TAG_subprogram'):
+                # print(dir(die.dwarfinfo))
+                print(get_subprogram(die, diedict, disassemble))
+
+
+def get_all_subprograms_eklavya_format(file_name):
+    elffile = get_ELFFile(file_name)
+    dwarfinfo = get_dwarfinfo(elffile)
+    diedict = get_offset2DIE(dwarfinfo)
+    disassemble = get_text_disassemble(elffile)
+    result = {}
+    for compile_unit in dwarfinfo.iter_CUs():
+        for die in compile_unit.iter_DIEs():
+            if (die.tag == 'DW_TAG_subprogram'):
+                subprogram = get_subprogram(die, diedict, disassemble)
+                result[subprogram['name']] = {
+                    'ret_type': subprogram['ret_type'],
+                    'args_type': subprogram['args_type'],
+                    'inst_bytes': subprogram['inst_bytes'],
+                    'boundaries': subprogram['boundaries'],
+                    'num_args': subprogram['num_args'],
+                    'inst_strings': None  # TODO: for the sake of completeness, fill this
+                }
+    return result
+
+
+def get_func_call_locations(file_name):
+    """
+    Generates a list dicts of the form  {call_address: integer, function_name: string}
+    which means that the instruction in address 'call_address' is a call to 'function_name'
+
+    The returned list is sorted in ascending address
+    """
+    result = []
+    disassemble_str = subprocess.check_output(
+        ['objdump', '--disassemble', file_name])
+    # look for lines like "0xblabla: byte byte byte byte... call address <function_name maybe@plt_something>"
+    for groups in re.findall(r'([a-f0-9]+):[^\n]+call[^\n]+<([a-zA-Z_][a-zA-Z_0-9]*)(?:\@[^\n]+?)?>', disassemble_str):
+        call_address = int(groups[0], 16)
+        call_name = groups[1]
+        # print(groups[0] + ' ' + groups[1])
+        if call_address in result:
+            raise Exception
+        else:
+            result.append({
+                'call_address': call_address,
+                'function_name': call_name
+            })
+
+    result.sort(lambda x, y: x['call_address'] - y['call_address'])
+    return result
+
+
+def get_offset2DIE(dwarfinfo):
+    """
+    Returns dict keyed by Compilation Unit offsets.
+    Each dict-value is another dict mapping an offset to a DIE.
+    """
+    result = {}
+    for compile_unit in dwarfinfo.iter_CUs():
+        result[compile_unit.cu_offset] = {}
+        # print('# CU ----------------------------')
+        for die in compile_unit.iter_DIEs():
+            # DIE attributes are CU-relative, thus the need for the subtraction
+            result[compile_unit.cu_offset][die.offset -
+                                           compile_unit.cu_offset] = die
+    return result
+
+
+def get_type(die, diedict):
+    """
+    Only some tags matter. Recurse to parse full type modifiers.
+
+    'diedict' is obtained with 'get_offset2DIE'
+
+    Returns the simplified name of the type (like int, void, or func_ptr)
+
+    See DWARF Format Version 5 section "5 Type Entries"
+    """
+
+    # Setup
+    typedict = {}
+    typedict['pointer_level'] = 0
+    typedict['func_ptr'] = False
+    typedict['base_name'] = None
+
+    def get_type_recursive(die, diedict, typedict):
+        if die.tag == 'DW_TAG_base_type':
+            typedict['base_name'] = die.attributes['DW_AT_name'].value
+
+        elif die.tag == 'DW_TAG_pointer_type':
+            typedict['pointer_level'] += 1
+            get_type_recursive(diedict[die.cu.cu_offset][die.attributes['DW_AT_type'].value],
+                               diedict, typedict)
+
+        elif die.tag == 'DW_TAG_typedef':
+            get_type_recursive(diedict[die.cu.cu_offset][die.attributes['DW_AT_type'].value],
+                               diedict, typedict)
+
+        elif die.tag == 'DW_TAG_structure_type':
+            struct_name = '<unknown>'
+            try:
+                struct_name = die.attributes['DW_AT_name'].value
+            except:
+                struct_name = '<unknown>'
+            typedict['base_name'] = 'struct ' + struct_name
+
+        elif die.tag == 'DW_TAG_union_type':
+            typedict['base_name'] = 'union ' + \
+                die.attributes['DW_AT_name'].value
+
+        elif die.tag == 'DW_TAG_enumeration_type':
+            typedict['base_name'] = 'enum ' + \
+                die.attributes['DW_AT_name'].value
+
+        elif die.tag == 'DW_TAG_subroutine_type':
+            typedict['func_ptr'] = True
+
+        elif die.tag == 'DW_TAG_array_type':
+            # raise Exception('Should not find any array types')
+            # typedefs may lead to array types as arguments in function signatures
+            # in practice, they are pointers
+            typedict['pointer_level'] += 1
+            get_type_recursive(diedict[die.cu.cu_offset][die.attributes['DW_AT_type'].value],
+                               diedict, typedict)
+
+        else:
+            subtype = None
+            try:
+                subtype = die.attributes['DW_AT_type'].value
+            except:
+                subtype = None
+            if subtype is not None:
+                get_type_recursive(diedict[die.cu.cu_offset][subtype],
+                                   diedict, typedict)
+            else:
+                # We have a void* in our hands =)
+                typedict['base_name'] = 'void'
+
+    get_type_recursive(die, diedict, typedict)
+
+    if (typedict['func_ptr']):
+        return 'func_ptr'
+    else:
+        return typedict['base_name'] + '*'*typedict['pointer_level']
+
+
+def get_subprogram(die, diedict, disassemble):
+    """
+    'diedict' is a mapping obtained with 'get_offset2DIE'
+    """
+    subroutine = {}
+    subroutine['name'] = die.attributes['DW_AT_name'].value
+    try:
+        subroutine['ret_type'] = get_type(
+            diedict[die.cu.cu_offset][die.attributes['DW_AT_type'].value], diedict)
+    except:
+        subroutine['ret_type'] = 'void'
+    subroutine['num_args'] = 0
+    subroutine['args_type'] = []
+
+    lowpc = die.attributes['DW_AT_low_pc'].value
+    # high_pc may either be relative or absolute
+    if die.attributes['DW_AT_high_pc'].form[:12] == 'DW_FORM_data':
+        highpc = lowpc + die.attributes['DW_AT_high_pc'].value - 1
+    else:
+        highpc = die.attributes['DW_AT_high_pc'].value - 1
+    subroutine['boundaries'] = (lowpc, highpc)
+
+    for parameter_die in die.iter_children():
+        if parameter_die.tag == 'DW_TAG_formal_parameter':
+            subroutine['num_args'] += 1
+            subroutine['args_type'].append(get_type(parameter_die, diedict))
+
+    subroutine['inst_strings'] = []
+    subroutine['inst_bytes'] = []
+    # TODO: this is too inefficient
+    for inst in disassemble:
+        if inst.address >= lowpc and inst.address <= highpc:
+            subroutine['inst_strings'].append(
+                inst.mnemonic + ' ' + inst.op_str)
+            subroutine['inst_bytes'].append(inst.bytes)
+
+    return subroutine
+
+
+def get_function_calls_eklavya_format(file_name):
+    """
+    Returns a dict function_name -> callers
+
+    in which 'callers' is an array of dicts with the form 
+    {
+        "caller": string,
+        "call_isntr_indices": array of integers
+    }
+    """
+    call_locations = get_func_call_locations(file_name)
+    subprograms = get_all_subprograms_eklavya_format(file_name)
+
+    # auxiliary array. contains boundaries in ascending order
+    subprogram_ranges = [{
+        'boundaries': subprograms[name]['boundaries'],
+        'name': name
+    } for name in subprograms.keys()]
+    subprogram_ranges.sort(
+        lambda x, y: x['boundaries'][0] - y['boundaries'][0])
+
+    # variable to be returned
+    result = {}
+
+    # filter out call locations which are not inside
+    # any regular function (those that, for example, are inside _start function)
+    tmp_call_locations = []
+    for call in call_locations:
+        caller = [subp for subp in subprogram_ranges if call['call_address']
+                  >= subp['boundaries'][0] and call['call_address'] <= subp['boundaries'][1]]
+        if len(caller) == 0:
+            continue
+        elif len(caller) > 1:
+            raise Exception('It seems functions overlap')
+        tmp_call_locations.append(call)
+    call_locations = tmp_call_locations
+
+    call_location_index = 0
+    for caller in subprogram_ranges:
+        appended = {}
+        call_location = call_locations[call_location_index]
+        while caller['boundaries'][0] <= call_location['call_address'] <= caller['boundaries'][1]:
+            try:
+                callers_array = result[call_location['function_name']]
+            except:
+                callers_array = result[call_location['function_name']] = []
+            if call_location['function_name'] not in appended:
+                appended[call_location['function_name']] = True
+                callers_array.append({
+                    'caller': caller['name'],
+                    'call_instr_indices': []
+                })
+            # Find instruction index
+            raw_inst_bytes = subprograms[caller['name']]['inst_bytes']
+            print(caller['name'])
+            print(subprograms[caller['name']])
+            print(call_location)
+            print('')
+            curr_address = caller['boundaries'][0]
+            index = 0
+            while curr_address != call_location['call_address']:
+                curr_address += len(raw_inst_bytes[index])
+                index += 1
+            callers_array[-1]['call_instr_indices'].append(index)
+            call_location_index += 1
+            if call_location_index == len(call_locations):
+                break
+            call_location = call_locations[call_location_index]
+        if call_location_index == len(call_locations):
+            break
+
+    return result
+
+
+def get_text_disassemble(elffile):
+    code = elffile.get_section_by_name('.text')
+    ops = code.data()
+    addr = code['sh_addr']
+    md = Cs(CS_ARCH_X86, CS_MODE_64)
+    return [i for i in md.disasm(ops, addr)]
+
+
+def test_details():
+    elffile = get_ELFFile('a.out')
+    code = elffile.get_section_by_name('.text')
+    ops = code.data()
+    addr = code['sh_addr']
+    md = Cs(CS_ARCH_X86, CS_MODE_64)
+    md.detail = True
+
+    for insn in md.disasm(ops, addr):
+        print("0x%x:\t%s\t%s" % (insn.address, insn.mnemonic, insn.op_str))
+        print(map(lambda x: to_x(int(x)), insn.bytes))
+        imm_count = insn.op_count(X86_OP_IMM)
+        if imm_count == 0:
+            continue
+        bytes_no_imm = []
+        # Inclusive 'start' and 'end' indexes
+        imm_start = insn.imm_offset
+        imm_end = imm_start + insn.imm_size + 1
+        for i in range(len(insn.bytes)):
+            if imm_start <= i <= imm_end:
+                continue
+            bytes_no_imm.append(insn.bytes[i])
+        print(map(lambda x: to_x(int(x)), bytes_no_imm))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if sys.argv[1] == '--test':
+            for filename in sys.argv[2:]:
+                print_debug(filename)
+        elif sys.argv[1] == '--objdump':
+            for filename in sys.argv[2:]:
+                get_func_call_locations(filename)
+        elif sys.argv[1] == '--functioncalls':
+            for filename in sys.argv[2:]:
+                print(get_function_calls_eklavya_format(filename))
+        elif sys.argv[1] == '--allsubprograms':
+            for filename in sys.argv[2:]:
+                print(get_all_subprograms_eklavya_format(filename))
+    else:
+        test_details()
